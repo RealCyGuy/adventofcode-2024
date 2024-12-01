8fec8151e4b9b7bf8ba3ce55752571cefd2efe5e
Cyrus Yip <cyrusyip@cyrusyip.com>
2024-12-01 01:44:48 -0500

    finish day 1

---
+++ day 01/part 2.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+
+from utils.api import get_input
+
+inp = get_input(1)
+list1 = []
+list2 = []
+for line in inp.splitlines():
+    split = line.split()
+    list1.append(int(split[0]))
+    list2.append(int(split[1]))
+list1.sort()
+list2.sort()
+similarity = 0
+for x, y in zip(list1, list2):
+    similarity += x * list2.count(x)
+print(similarity)
